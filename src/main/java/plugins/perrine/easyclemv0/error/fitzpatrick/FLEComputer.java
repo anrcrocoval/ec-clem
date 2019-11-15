e148f5ff8c226b6889cedf4b98363846e3ca856e
Gpotier <guillaume.potier@univ-nantes.fr>
2019-11-15 17:06:14 +0100

    Remove Ipopt Fixture + Add missing copyright notice

--- src/main/java/plugins/perrine/easyclemv0/error/fitzpatrick/FLEComputer.java
+++ src/main/java/plugins/perrine/easyclemv0/error/fitzpatrick/FLEComputer.java
@@ -1,22 +1,15 @@
-/*
- * Copyright 2010-2018 Perrine Paul-Gilloteaux, CNRS.
+/**
+ * Copyright 2010-2018 Perrine Paul-Gilloteaux <Perrine.Paul-Gilloteaux@univ-nantes.fr>, CNRS.
+ * Copyright 2019 Guillaume Potier <guillaume.potier@univ-nantes.fr>, INSERM.
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of EC-CLEM.
+ *
+ * you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-
-
+ *
+ **/
 package plugins.perrine.easyclemv0.error.fitzpatrick;
 
 import javax.inject.Inject;
